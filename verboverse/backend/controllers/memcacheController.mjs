320e2657b50e9f9c65b05c8aa9519fa552faa4df
Vanshika V <vansvirm@gmail.com>
2023-11-28 02:15:01 -0500

    cacheData works

--- verboverse/backend/controllers/memcacheController.mjs
+++ verboverse/backend/controllers/memcacheController.mjs
@@ -6,16 +6,21 @@ let keyCounter = 1;
 export async function cacheData (req, res, next) {
     const key = `${keyCounter++}`;
 
-    console.log(key)
     try {
         const value = req.body.value;
-        await memcachedClient.set(key, value, function (error, result) {
-            if (error) {
-                console.error(error);
+        memcachedClient.set(key, value, 0, function (err) {
+            if (err) {
+                return res.status(500).json({
+                    success: false,
+                    error: err
+                });
+            } else {
+            return res.status(201).json({
+                success: true,
+                key
+            });
             }
-            console.log(result);
-        } , {expires: 600});
-        console.log("HI")
+       });
 
     } catch (error) {
         next(error);
--- verboverse/backend/models/memcache.mjs
+++ verboverse/backend/models/memcache.mjs
@@ -1,10 +1,15 @@
-import Memcached from 'memjs';
+import Memcached from "memcached";
 
 // Initialize Memcached
-const memcachedClient = Memcached.Client.create(process.env.MEMCACHIER_SERVERS, {
-    failover: true,  // default: false
-    timeout: 1,      // default: 0.5 (seconds)
-    keepAlive: true  // default: false
-}); 
+// const memcachedClient = Memcached.Client.create(process.env.MEMCACHIER_SERVERS, {
+//     failover: true,  // default: false
+//     timeout: 1,      // default: 0.5 (seconds)
+//     keepAlive: true  // default: false
+// }); 
+
+
+console.log(process.env.MEMCACHIER_SERVERS); 
+
+const memcachedClient = new Memcached('localhost:11211');
 
 export default memcachedClient;
