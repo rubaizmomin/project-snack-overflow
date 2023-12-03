fd7894367663950262113b5b75675725a09f90c5
Vanshika V <vansvirm@gmail.com>
2023-12-03 17:14:42 -0500

    Things seem to be working

--- verboverse/backend/routes/authRoutes.mjs
+++ verboverse/backend/routes/authRoutes.mjs
@@ -5,7 +5,7 @@ const router = express.Router();
 
 router.post('/signup', signup);
 router.post('/signin', signin); 
-router.get('/logout', logout);
+router.get('/logout', isAuthenticated, logout);
 router.get('/me', isAuthenticated, userProfile); 
 
 export const authRoutes = router;
--- verboverse/backend/routes/sendgridRoutes.mjs
+++ verboverse/backend/routes/sendgridRoutes.mjs
@@ -1,7 +1,8 @@
 import express from 'express';
 import { sendEmail } from '../controllers/sendgridController.mjs';
+import { isAuthenticated } from '../middleware/auth.mjs';
 const router = express.Router();
 
-router.post('/sendEmail', sendEmail);
+router.post('/sendEmail', isAuthenticated, sendEmail);
 
 export const sendgridRoutes = router;
